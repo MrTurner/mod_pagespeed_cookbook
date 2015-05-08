5f890f288ca2dec0152cd4e59ef3130157666a18
Taylor Price <tayworm@gmail.com>
2015-05-08 00:48:18 -0700

    initial port from core apache2 cookbook

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'mod_pagespeed'
+maintainer       'Taylor Price'
+maintainer_email 'tayworm@gmail.com'
+license          'apache2'
+description      'Installs/Configures mod_pagespeed'
+long_description 'Installs/Configures mod_pagespeed'
+version          '0.1.0'
+
+depends 'apache2'
