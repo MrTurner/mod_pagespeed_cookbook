3d8b21eff153b542e76da7a2bea1ea2aa8ae1bbb
Taylor Price <tayworm@gmail.com>
2015-05-13 22:34:35 -0700

    updating metadata for supermarket

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,17 @@
 name             'mod_pagespeed'
 maintainer       'Taylor Price'
 maintainer_email 'tayworm@gmail.com'
-license          'apache2'
-description      'Installs/Configures mod_pagespeed'
-long_description 'Installs/Configures mod_pagespeed'
+issues_url 'https://github.com/drpebcak/mod_pagespeed_cookbook/issues'
+source_url 'https://github.com/drpebcak/mod_pagespeed_cookbook'
+license          'Apache 2.0'
+description      'Installs/Configures mod_pagespeed for use with apache'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
 depends 'apache2'
+
+supports 'debian'
+supports 'ubuntu'
+supports 'redhat'
+supports 'centos'
+supports 'amazon'
