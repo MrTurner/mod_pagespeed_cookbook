260cd334a9f17442a114fb119295a77c24c3da31
Taylor Price <tayworm@gmail.com>
2015-10-22 08:37:14 -0700

    gear up for major release 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ source_url 'https://github.com/drpebcak/mod_pagespeed_cookbook'
 license          'Apache 2.0'
 description      'Installs/Configures mod_pagespeed for use with apache'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '1.0.0'
 
 depends 'apache2'
 
